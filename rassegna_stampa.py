456bd679ace9f96efda70ce64c9baf07d3939a91
megaics <megaics@gmail.com>
2018-10-15 23:07:22 +0200

    Ora la rassegna mette le foto in un album

--- rassegna_stampa.py
+++ rassegna_stampa.py
@@ -8,17 +8,39 @@ from lxml import html
 import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
+import urllib
 
 URL = 'http://www.giornalone.it/quotidiani_italiani/'
 base_url = 'http://www.giornalone.it'
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'}
-ci_devono_essere = ['/prima_pagina_libero/', '/prima_pagina_la_verita/', '/prima_pagina_il_giornale/', '/prima_pagina_il_tempo/']
-nomi_giornali = ['Libero', 'La verità', 'Il giornale', 'Il tempo']
-url_prime_pagine_caso = ['/prima_pagina_il_fatto_quotidiano/', '/prima_pagina_la_repubblica/', '/prima_pagina_il_messaggero/', '/prima_pagina_il_mattino/', '/prima_pagina_il_gazzettino/', '/prima_pagina_la_stampa/', '/prima_pagina_il_manifesto/', '/prima_pagina_il_secolo_xix/', '/prima_pagina_la_gazzetta_del_mezzogiorno/', '/prima_pagina_il_resto_del_carlino/', '/prima_pagina_l_unione_sarda/', '/prima_pagina_avvenire/', '/prima_pagina_l_osservatore_romano/']
-TOKEN = 'il_token'
+headers = {'User-Agent':\
+	'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'}
+
+giornali = {\
+'Libero': '/prima_pagina_libero',\
+'La verità': '/prima_pagina_la_verita/',\
+'Il giornale': '/prima_pagina_il_giornale/',\
+'Il tempo': '/prima_pagina_il_tempo/',\
+'Il fatto quotidiano': '/prima_pagina_il_fatto_quotidiano/',\
+'La repubblica': '/prima_pagina_la_repubblica/',\
+'Il messaggero': '/prima_pagina_il_messaggero/',\
+'Il mattino': '/prima_pagina_il_mattino/',\
+'Il gazzettino': '/prima_pagina_il_gazzettino/',\
+'La stampa': '/prima_pagina_la_stampa/',\
+'Il manifesto': '/prima_pagina_il_manifesto/',\
+'Il secolo XIX': '/prima_pagina_il_secolo_xix/',\
+'La gazzetta del mezzogiorno': '/prima_pagina_la_gazzetta_del_mezzogiorno/',\
+'Il resto del carlino': '/prima_pagina_il_resto_del_carlino/',\
+'L\'unione sarda': '/prima_pagina_l_unione_sarda/',\
+'Avvenire': '/prima_pagina_avvenire/',\
+'L\'Osservatore romano': '/prima_pagina_l_osservatore_romano/',\
+'Il dubbio': '/prima_pagina_il_dubbio/',\
+'La notizia': '/prima_pagina_la_notizia/'\
+}
+
+TOKEN = 'IL TOKEN'
 API_URL = 'https://api.telegram.org/bot%s' % (TOKEN)
-id_canale = ID_DEL_CANALE
-id_mio = ID_MIO_PER_I_TEST
+id_canale = 666
+id_mio = 666
 GOOGLE_TRANSLATE = 1
 
 
@@ -44,70 +66,102 @@ def requests_retry_session(
 GLOBXPATH=None
 def get_proxy_url(url, proxy=None):
 	if proxy == GOOGLE_TRANSLATE:
-		response_google_translate = requests_retry_session().get('https://translate.google.com/translate?sl=en&tl=it&js=y&prev=_t&hl=it&ie=UTF-8&u=%s&edit-text=&act=url' % (url), headers=headers)
+		response_google_translate = requests_retry_session().\
+				get('https://translate.google.com/translate?sl=en&\
+tl=it&js=y&prev=_t&hl=it&ie=UTF-8&u=%s&edit-text=&\
+act=url' % (url), headers=headers)
 		if response_google_translate.status_code == 403:
-			send_message(id_mio, '403 FORBIDDEN GOOGLE TRANSLATE PRIMA PASSATA')
-		xpath_google_translate = html.fromstring(response_google_translate.content)
-		url_reale = xpath_google_translate.xpath('//div[@id="contentframe"]//iframe/@src')[0]
-		response_google_translate = requests_retry_session().get(url_reale, headers=headers)
+			send_message(id_mio, '403 FORBIDDEN GOOGLE TRANSLATE\
+					PRIMA PASSATA')
+		xpath_google_translate = html.fromstring(\
+				response_google_translate.content)
+		url_reale = xpath_google_translate.\
+				xpath('//div[@id="contentframe"]//iframe/@src')[0]
+		response_google_translate =\
+				requests_retry_session().get(url_reale, headers=headers)
 		if response_google_translate.status_code == 403:
-			send_message(id_mio, '403 FORBIDDEN GOOGLE TRANSLATE SECONDA PASSATA')
+			send_message(id_mio, '403 FORBIDDEN GOOGLE TRANSLATE\
+					SECONDA PASSATA')
 		html_google= response_google_translate.content
 		xpath_google_translate = html.fromstring(html_google)
 		return xpath_google_translate.xpath('//a/@href')[0]
 	
 	return url # proxy == None
 
-def send_message(chat_id, text):
-	response_message = requests_retry_session().get('%s/sendMessage?chat_id=%s&text=%s' % (API_URL, chat_id, text))
+def my_json_serialize(l):
+	big_string = '['
+	for x in l:
+		big_string += '{"type": "photo", "media": "%s"},' % (x)
+	big_string = big_string[:-1] + ']'
+	return urllib.quote(big_string)
+
+def send_message(chat_id, text, markdown=False):
+	req_url = '%s/sendMessage?chat_id=%s&text=%s'\
+			% (API_URL, chat_id, text)
+	if markdown:
+		req_url += '&parse_mode=Markdown'
+	response_message = requests_retry_session().get(req_url)
 
 def send_photo_url(chat_id, url_photo):
-	response_message = requests_retry_session().get('%s/sendPhoto?chat_id=%s&photo=%s' % (API_URL, chat_id, url_photo))
+	req_url = '%s/sendPhoto?chat_id=%s&photo=%s'\
+			% (API_URL, chat_id, url_photo)
+	response_message = requests_retry_session().get(req_url)
+
+def send_media_group(chat_id, media_group):
+	req_url = '%s/sendMediaGroup?chat_id=%s&media=%s'\
+			% (API_URL, chat_id, media_group)
+	response_message = requests_retry_session().get(req_url)
+
+def send_rassegna(chat_id, img_list, old_list):
+	send_message(chat_id, '*== RASSEGNA GIORNO %s ==*'\
+			% str(datetime.date.today()), True)
+	
+	# feature non voluta
+	# for old in old_list:
+		# send_message(chat_id,\
+			# '\"%s\" ha un\'edizione vecchia e quindi non lo visualizzo'\
+			# % old)
+	
+	for i in range(0, len(img_list), 10):
+		send_media_group(chat_id, my_json_serialize(img_list[i:i+10]))
 
 def scarica_rassegna(test=False, proxy=None):
 	id_chat = id_canale
 	if test:
 		id_chat = id_mio
 	
-	url_prime_pagine = ci_devono_essere
-	for url_pp in url_prime_pagine:
+	img_list = []
+	old_list = []
+	
+	for name_pp, url_pp in giornali.items():
 		url_giornale = base_url + url_pp
-		giornale_response = requests_retry_session().get(get_proxy_url(url_giornale, proxy=proxy), headers=headers)
+		giornale_response = requests_retry_session().get(\
+				get_proxy_url(url_giornale, proxy=proxy), headers=headers)
 		if giornale_response.status_code == 403:
 			send_message(id_mio, '403 FORBIDDEN DI %s' %(url_giornale))
 		giornale_content = html.fromstring(giornale_response.content)
 		giornale_content = giornale_content.xpath('//div[@id="left"]')[0];
-		if len(giornale_content.xpath('//div[contains(@class, "edition-bar-old")]')) > 0:
+		if len(giornale_content.\
+				xpath('//div[contains(@class, "edition-bar-old")]')) > 0:
 			# il giornale e' vecchio
-			nome_giornale = nomi_giornali[url_prime_pagine.index(url_pp)]
-			send_message(id_chat, '"%s" c\'ha un\'edizione vecchia e quindi non lo visualizzo.' % (nome_giornale))
+			old_list.append(name_pp)
 			continue
-		url_img = base_url + giornale_content.xpath('//div[@id="giornale-wrap"]/img/@src')[0]
-		send_photo_url(id_chat, url_img)
+		url_img = base_url +giornale_content.\
+				xpath('//div[@id="giornale-wrap"]/img/@src')[0]
+		ls = url_img.rfind("/") # last_slash
+		url_img = url_img[:ls+1] + "." + url_img[ls:]
+		img_list.append(url_img)
 		time.sleep(5)
 	
-	while True:
-		if len(url_prime_pagine_caso) == 0:
-			send_message(id_chat, 'Nessuna pagina random con edizione nuova')
-			break
-		url_pagina_random = random.choice(url_prime_pagine_caso)
-		url_prime_pagine_caso.remove(url_pagina_random)
-		url_giornale = base_url + url_pagina_random
-		giornale_response = requests_retry_session().get(get_proxy_url(url_giornale, proxy=proxy), headers=headers)
-		if giornale_response.status_code == 403:
-			send_message(id_mio, '403 FORBIDDEN DI PAGINA RANDOM %s' %(url_giornale))
-		giornale_content = html.fromstring(giornale_response.content)
-		giornale_content = giornale_content.xpath('//div[@id="left"]')[0];
-		if len(giornale_content.xpath('//div[contains(@class, "edition-bar-old")]')) > 0:
-			# il giornale e' vecchio
-			continue
-		url_img = base_url + giornale_content.xpath('//div[@id="giornale-wrap"]/img/@src')[0]
-		send_photo_url(id_chat, url_img)
-		break
+	send_rassegna(id_chat, img_list, old_list)
 
 def main():
+	# SEMMAI un giorno riuscissi a far funzionare quest'obbrobbrio
+	# senza cron quello sara' un bel giorno visto che potro' implementare
+	# anche dei comandi tipo
 	orario_pubblicazione = datetime.time(7, 0, 0)
-	print('Orario pubblicazione: %02d:%02d:%02d' %(orario_pubblicazione.hour, orario_pubblicazione.minute, orario_pubblicazione.second))
+	print('Orario pubblicazione: %02d:%02d:%02d' %(orario_pubblicazione.hour\
+			, orario_pubblicazione.minute, orario_pubblicazione.second))
 	
 	dt = datetime.datetime.now()
 	hour = dt.hour
@@ -133,4 +187,4 @@ def test():
 	scarica_rassegna(test=True, proxy=GOOGLE_TRANSLATE)
 
 if __name__ == '__main__':
-	main()
+	scarica_rassegna(test=False, proxy=GOOGLE_TRANSLATE)
